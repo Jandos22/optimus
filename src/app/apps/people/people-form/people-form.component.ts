a14ed122431b7a0b3f7213bf1bd1d72cc0d0c9df
ombayev <ombayev@gmail.com>
2017-12-03 13:42:37 +0500

    sp upload and item create works

--- src/app/apps/people/people-form/people-form.component.ts
+++ src/app/apps/people/people-form/people-form.component.ts
@@ -153,9 +153,9 @@ export class PeopleFormComponent implements OnInit, OnDestroy {
   }
 
   _base64ToArrayBuffer(base64) {
-    let binary_string =  window.atob(base64);
-    let len = binary_string.length;
-    let bytes = new Uint8Array( len );
+    const binary_string =  window.atob(base64);
+    const len = binary_string.length;
+    const bytes = new Uint8Array( len );
     for (let i = 0; i < len; i++)        {
         bytes[i] = binary_string.charCodeAt(i);
     }
