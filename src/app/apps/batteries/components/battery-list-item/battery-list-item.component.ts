78189c1518a90faa91abc0a76ebd1241933234c6
Zhandos Ombayev <ZOmbayev@slb.com>
2019-06-08 16:30:42 +0500

    fixed battery background color when it is disposed

--- src/app/apps/batteries/components/battery-list-item/battery-list-item.component.ts
+++ src/app/apps/batteries/components/battery-list-item/battery-list-item.component.ts
@@ -4,17 +4,17 @@ import {
   Output,
   EventEmitter,
   ViewEncapsulation
-} from "@angular/core";
+} from '@angular/core';
 
-import * as _ from "lodash";
+import * as _ from 'lodash';
 
 // interfaces
-import { BatteryItem } from "../../../../shared/interface/batteries.model";
+import { BatteryItem } from '../../../../shared/interface/batteries.model';
 
 @Component({
-  selector: "app-battery-list-item",
-  styleUrls: ["battery-list-item.component.scss"],
-  templateUrl: "battery-list-item.component.html",
+  selector: 'app-battery-list-item',
+  styleUrls: ['battery-list-item.component.scss'],
+  templateUrl: 'battery-list-item.component.html',
   encapsulation: ViewEncapsulation.Emulated
 })
 export class BatteryListItemComponent {
@@ -33,51 +33,56 @@ export class BatteryListItemComponent {
     if (_.isNumber(hours)) {
       return `${70 - hours} hour(s) left`;
     } else {
-      return "unknown";
+      return 'unknown';
     }
   }
 
   getBatteryIcon(hours: number) {
     if (hours === 0) {
-      return "battery-full";
+      return 'battery-full';
     }
 
     if (hours > 0 && hours < 17.5) {
-      return "battery-three-quarters";
+      return 'battery-three-quarters';
     }
 
     if (hours >= 17.5 && hours < 35) {
-      return "battery-half";
+      return 'battery-half';
     }
 
     if (hours >= 35 && hours < 52.5) {
-      return "battery-quarter";
+      return 'battery-quarter';
     }
 
     if (hours >= 52.5) {
-      return "battery-empty";
+      return 'battery-empty';
     }
   }
 
-  getBatteryLevel(hours: number) {
-    // recieve hours used and depending on that,
-    // return a class name that would then used to specify background color
-    return hours === 0
-      ? "battery-new"
-      : hours > 0 && hours < 70
-      ? "battery-has-charge"
-      : (hours => 70)
-      ? "battery-no-charge"
-      : "not a valid number";
+  getBatteryLevel(hours: number, status: string) {
+    console.log(status);
+    if (status === 'Disposed') {
+      return 'battery-no-charge';
+    } else {
+      // recieve hours used and depending on that,
+      // return a class name that would then used to specify background color
+      return hours === 0
+        ? 'battery-new'
+        : hours > 0 && hours < 70
+        ? 'battery-has-charge'
+        : (hours => 70)
+        ? 'battery-no-charge'
+        : 'not a valid number';
+    }
   }
 
   getChipColor(status: string) {
-    return status === "New"
-      ? ""
-      : status === "InUse"
-      ? "primary"
-      : status === "FmtOut" || status === "Disposed"
-      ? "accent"
-      : "";
+    return status === 'New'
+      ? ''
+      : status === 'InUse'
+      ? 'primary'
+      : status === 'FmtOut' || status === 'Disposed'
+      ? 'accent'
+      : '';
   }
 }
